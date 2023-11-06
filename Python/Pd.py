406e5aa2648572f2ac24a07c7c4c0b18b1b968b5
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-11-06 18:46:20 +0100

    Update Pd.py

--- Python/Pd.py
+++ Python/Pd.py
@@ -1,13 +1,16 @@
-import math
-n = int(input('Podaj n: '))
-a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
-x = int(input('Podaj x: '))
+n = 100
+i = 1
+a = []
+for i in range (n):
+    a.append(i)
+    i += 1
+x = 2
 w = a[0]
 ilosc = 0
-i = 1
-for i in a:
-        w = w * x +a[i]
-        i = i + 1
-        ilosc += 2
+j = 1
+for i in range (1,n):
+        w = w * x +a[j]
+        j = j + 1
+        ilosc += j
 print(w)
 print(ilosc)
